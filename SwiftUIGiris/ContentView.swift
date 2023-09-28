1ffd601cfab40adc5862c1d0c86f5f9efe1d03a7
Pelin AYBAR <aybarpelin00@gmail.com>
2023-09-28 13:54:52 +0300

    Counter

--- SwiftUIGiris/ContentView.swift
+++ SwiftUIGiris/ContentView.swift
@@ -1,21 +1,50 @@
-//
-//  ContentView.swift
-//  SwiftUIGiris
-//
-//  Created by Pelin Ay on 28.09.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 10){
+            HStack {
+                Rectangle().fill(Color.red).frame(width: 30, height: 30)
+                Text("Counter: \(counter)")
+                    .font(.system(size: 35))
+                    .foregroundColor(.purple)
+            }
+            .padding(30)
+            HStack{
+                Button(action: {
+                    counter = counter + 1
+                }) {
+                    Text("Increase")
+                        .font(.system(size: 25))
+                        .frame(width: 150, height: 50) // Buton boyutu
+                        .background(Color.green) // Buton arka plan rengi
+                        .foregroundColor(.white) // Buton metin rengi
+                        .cornerRadius(10) // Buton köşe yuvarlama
+                }
+                Button(action: {
+                    counter = counter - 1
+                }) {
+                    Text("Decrease")
+                        .font(.system(size: 25))
+                        .frame(width: 150, height: 50)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            Button(action: {
+                counter = 0
+            }) {
+                Text("Reset")
+                    .font(.system(size: 25))
+                    .frame(width: 100, height: 50)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
-        .padding()
     }
 }
 
--- SwiftUIGiris/SwiftUIGirisApp.swift
+++ SwiftUIGiris/SwiftUIGirisApp.swift
@@ -1,10 +1,3 @@
-//
-//  SwiftUIGirisApp.swift
-//  SwiftUIGiris
-//
-//  Created by Pelin Ay on 28.09.2023.
-//
-
 import SwiftUI
 
 @main
