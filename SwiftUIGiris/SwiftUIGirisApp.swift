ca13f4f3a6360024e661a66280d14bf41c09e64c
Pelin AYBAR <aybarpelin00@gmail.com>
2023-09-28 11:38:46 +0300

    Initial Commit

---
+++ SwiftUIGiris/SwiftUIGirisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGirisApp.swift
+//  SwiftUIGiris
+//
+//  Created by Pelin Ay on 28.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGirisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
